6f0382d0344eefd2db2f03213c44b8508b34641d
CsRobi <csillagrobert95@gmail.com>
2020-05-26 11:32:05 +0300

    Initial commit

---
+++ vault-config-server/src/main/java/com/jlr/rcsillag/vault/configserver/VaultConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.jlr.rcsillag.vault.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class VaultConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultConfigServerApplication.class, args);
+	}
+
+}
