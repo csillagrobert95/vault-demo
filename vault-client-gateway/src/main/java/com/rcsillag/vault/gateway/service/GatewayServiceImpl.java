f4218c44f7d59cfd5f8df9731461eeecde8c3bbe
CsRobi <csillagrobert95@gmail.com>
2020-06-08 14:35:21 +0300

    Externalize base URL config for vault-client-gateway

--- vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/service/GatewayServiceImpl.java
+++ vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/service/GatewayServiceImpl.java
@@ -1,5 +1,6 @@
 package com.rcsillag.vault.gateway.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,9 +11,10 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GatewayServiceImpl implements GatewayService {
     /**
-     * Base URL to call.
+     * External service base URL.
      */
-    private static final String URL = "https://10.128.0.3:8443";
+    @Value("${external.service.base.url:http://localhost:8443}")
+    private String baseUrl;
 
     /**
      * The RestTemplate to use.
@@ -33,7 +35,7 @@ public class GatewayServiceImpl implements GatewayService {
     public String sayHello(String name){
         String responseString;
         try {
-            String helloUrl = URL + "/hello/" + name ;
+            String helloUrl = baseUrl + "/hello/" + name ;
             ResponseEntity<String> response
                     = restTemplate.getForEntity(helloUrl, String.class);
             responseString =  response.getBody();
