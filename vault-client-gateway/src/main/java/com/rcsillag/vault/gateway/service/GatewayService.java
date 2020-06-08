7d8d5d86e5585066079213bc37fa1d9f5947906a
CsRobi <csillagrobert95@gmail.com>
2020-06-08 12:40:18 +0300

    Extract GatewayService interface

--- vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/service/GatewayService.java
+++ vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/service/GatewayService.java
@@ -1,47 +1,13 @@
 package com.rcsillag.vault.gateway.service;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 /**
- * Service for calling external REST endpoint.
+ * This interface defines the methods for forwarding requests to external services.
  */
-@Service
-public class GatewayService {
+public interface GatewayService {
     /**
-     * Base URL to call.
-     */
-    private static final String URL = "https://10.128.0.3:8443";
-
-    /**
-     * The RestTemplate to use.
-     */
-    private RestTemplate restTemplate;
-
-    /**
-     * Constructor.
-     */
-    public GatewayService() {
-        restTemplate = new RestTemplate();
-    }
-
-    /**
-     * Call external service and return the response String.
+     * Call external greeting service and return the response String.
      * @param name The name to greet.
      * @return The response String.
      */
-    public String sayHello(String name){
-        String responseString;
-        try {
-            String helloUrl = URL + "/hello/" + name ;
-            ResponseEntity<String> response
-                    = restTemplate.getForEntity(helloUrl, String.class);
-            responseString =  response.getBody();
-        }catch (Exception e){
-            responseString = "Something went wrong: " + e.getMessage();
-            e.printStackTrace();
-        }
-        return responseString;
-    }
+    String sayHello(String name);
 }
---
+++ vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/service/GatewayServiceImpl.java
@@ -0,0 +1,46 @@
+package com.rcsillag.vault.gateway.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Service for calling external REST endpoint.
+ */
+@Service
+public class GatewayServiceImpl implements GatewayService {
+    /**
+     * Base URL to call.
+     */
+    private static final String URL = "https://10.128.0.3:8443";
+
+    /**
+     * The RestTemplate to use.
+     */
+    private RestTemplate restTemplate;
+
+    /**
+     * Constructor.
+     */
+    public GatewayServiceImpl() {
+        restTemplate = new RestTemplate();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String sayHello(String name){
+        String responseString;
+        try {
+            String helloUrl = URL + "/hello/" + name ;
+            ResponseEntity<String> response
+                    = restTemplate.getForEntity(helloUrl, String.class);
+            responseString =  response.getBody();
+        }catch (Exception e){
+            responseString = "Something went wrong: " + e.getMessage();
+            e.printStackTrace();
+        }
+        return responseString;
+    }
+}
