8beaae522f84a6c925a96768b5b68bc655f14a4a
CsRobi <csillagrobert95@gmail.com>
2020-06-09 18:38:36 +0300

    Get certificate stored in vault secret for vault-client-gateway

--- vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/VaultClientGatewayApplication.java
+++ vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/VaultClientGatewayApplication.java
@@ -1,12 +1,15 @@
 package com.rcsillag.vault.gateway;
 
+import com.rcsillag.vault.gateway.config.VaultCertificateConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * Main class of the vault-client-gateway module.
  */
 @SpringBootApplication
+@EnableConfigurationProperties(VaultCertificateConfig.class)
 public class VaultClientGatewayApplication {
 
 	public static void main(String[] args) {
---
+++ vault-client-gateway/src/main/java/com/rcsillag/vault/gateway/config/VaultCertificateConfig.java
@@ -0,0 +1,49 @@
+package com.rcsillag.vault.gateway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Domain model for TLS certificate data stored in Vault.
+ */
+@ConfigurationProperties("cert")
+public class VaultCertificateConfig {
+    private String serialNumber;
+
+    private String certificate;
+
+    private String issuingCaCertificate;
+
+    private String privateKey;
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public String getIssuingCaCertificate() {
+        return issuingCaCertificate;
+    }
+
+    public void setIssuingCaCertificate(String issuingCaCertificate) {
+        this.issuingCaCertificate = issuingCaCertificate;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+}
